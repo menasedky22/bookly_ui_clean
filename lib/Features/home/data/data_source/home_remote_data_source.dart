27127445884992707117109743fc2ec02b890d49
mena <menasedky22@gmail.com>
2024-01-19 13:30:34 +0200

    create home remote data source

---
+++ lib/Features/home/data/data_source/home_remote_data_source.dart
@@ -0,0 +1,20 @@
+import '../../domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeatureBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  @override
+  Future<List<BookEntity>> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
