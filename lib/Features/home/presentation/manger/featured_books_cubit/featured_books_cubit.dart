8f243b469206dedf67a5e3fbc938628af54cdf4f
mena <menasedky22@gmail.com>
2024-01-20 14:51:28 +0200

    create newest books cubit

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -10,7 +10,7 @@ class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
       : super(FeaturedBooksInitial());
 
   final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
-  Future<void> fetchTeaturedBooks() async {
+  Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
 
     var result = await fetchFeaturedBooksUseCase.call();
---
+++ lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/use_cases/fetch_newest_books_use_case.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchnewestBooks() async {
+    emit(NewestBooksLoading());
+
+    var result = await fetchNewestBooksUseCase.call();
+
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.message));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
